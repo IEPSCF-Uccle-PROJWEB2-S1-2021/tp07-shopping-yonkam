a98b7adf9df5452aef45392ce670bd5d4ded9170
yonkam <71528448+yonkam@users.noreply.github.com>
2020-10-26 16:52:25 +0100

    tp7

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 // Écrivez votre code JavaScript ici.
+ const ul =  document.querySelector('ul') ;
+ const  inserrer  =  document.querySelector ( 'input' ) ;
+ const bouton=  document.querySelector('button') ;
+
+ bouton . onclick  =  function ( )  {
+  let valeurcourante  =  inserrer.value ;
+  inserrer.value  =  "" ;
+ const li = document.createElement('li');
+ const span = document.createElement('span');
+ const boutton = document.createElement('button');
+
+ li. appendChild ( span) ;
+ li. appendChild ( boutton ) ;
+ span.textContent= valeurcourante;
+ boutton.textContent= "delete";
+ ul.appendChild(li);
+
+
+ boutton. onclick  =  function(){
+ ul . removeChild ( li ) ;
+input . focus ( ) ;
+ }
+
+}
